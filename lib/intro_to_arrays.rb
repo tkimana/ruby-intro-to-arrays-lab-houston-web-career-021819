22c179b22589cd7c74b75652419bec2490daecda
Theodore Kimana <kimana.theodore@gmail.com>
2019-02-08 01:17:07 +0000

    Done.

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,13 @@
+def instantiate_new_array
+ @my_name_array = ["Theodore"]
+end
+def instantiate_new_array
+[]
+end
+def array_with_two_elements
+  @my_two_array = ["Theodore", "James"]
+end
+def my_first_element
+  my_first_element.index ("Theodore")
+end
+  
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,6 @@
-
+def my_first_element
+@taylor_swift.index = ["Theodore"]
+end
 RSpec.configure do |config|
 
   def __
