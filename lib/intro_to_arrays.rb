23d84e6c9b704d2ce16c68e9a0393c37bc7a7541
Theodore Kimana <kimana.theodore@gmail.com>
2019-02-08 03:06:28 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -7,7 +7,20 @@ end
 def array_with_two_elements
   @my_two_array = ["Theodore", "James"]
 end
-def my_first_element
-  my_first_element.index ("Theodore")
+def first_element(array)
+array[0]
 end
-  
+  def third_element(array)
+    array[2]
+  end
+  def last_element(array)
+    array[-1]
+  end
+  def first_element_with_array_methods(array)
+   first_country  = "Thailand"
+  end
+  def last_element_with_array_methods(array)
+    last_country = "Myanmar"
+  end
+  def length_of_array(array)
+    
